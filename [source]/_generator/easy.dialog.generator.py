7389b6a0c414f467de7ca5b1d41f525186445d85
aleksversus <aleksversus@mail.ru>
2023-11-26 15:36:22 +0500

    refactor: replace output path in variable.

--- [source]/_generator/easy.dialog.generator.py
+++ [source]/_generator/easy.dialog.generator.py
@@ -1,15 +1,17 @@
 from easy.microbase import DialogsBase
 
 def main() -> None:
-	# составляем список диалогов из которых будем формировать базу
+	# составляем список диалогов из которых будем формировать таблицу данных
 	dialogs_files = [
 		'..\\..\\[source.game]\\other\\bar.barmen.edg',
 		'..\\..\\[source.game]\\other\\bar.alcoholic.edg',
 		'..\\..\\[source.game]\\other\\bar.aragorn.edg',
 		'..\\..\\[source.game]\\other\\robbank.edg'
 	]
+	# Выходной файл
+	output_path = "..\\..\\[source.game]\\src\\dialogs_table.qsps"
 	# формируем объект БазаДиалогов, и генерируем в нём список объектов диалогов
-	eid = DialogsBase(dialogs_files, split_code=10, output_path="..\\..\\[source.game]\\src\\dialogs_table.qsps")
+	eid = DialogsBase(dialogs_files, split_code=10, output_path=output_path)
 	# теперь конвертируем диалоги из базы в Таблицу Данных для QSP
 	eid.to_qsps()
 
